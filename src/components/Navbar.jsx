e99b1abcfb2470adc9bd8b17f19d578b2aac66fb
wang4621 <82539201+wang4621@users.noreply.github.com>
2023-06-02 00:21:42 -0700

    c

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,10 +2,10 @@ import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { styles } from "../styles";
 import { navLinks } from "../constants";
-import { logo, menu, close } from "../assets";
+import { menu, close } from "../assets";
 
 const Navbar = () => {
-  const [active, setActive] = useState("");
+  // const [active, setActive] = useState("");
   const [toggle, setToggle] = useState(false);
   // const [scrolled, setScrolled] = useState(false);
 
@@ -24,11 +24,12 @@ const Navbar = () => {
   //   return () => window.removeEventListener("scroll", handleScroll);
   // }, []);
 
+  const animate1 = toggle ? "rotate-45 translate-y-1.5" : "";
+  const animate2 = toggle ? "-rotate-45 -translate-y-1" : "";
+
   return (
     <nav
-      className={`${
-        styles.paddingX
-      } w-full flex items-center py-5 fixed top-0 z-20`}
+      className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20`}
       // ${scrolled ? "bg-primary" : "bg-transparent"}
     >
       <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
@@ -40,7 +41,6 @@ const Navbar = () => {
             window.scrollTo(0, 0);
           }}
         >
-          {/* <img src={logo} alt="logo" className="w-9 h-9 object-contain" /> */}
           <p className="text-white text-[18px] font-bold cursor-pointer flex">
             Home
           </p>
@@ -49,22 +49,26 @@ const Navbar = () => {
           {navLinks.map((nav) => (
             <li
               key={nav.id}
-              className={`${
-                active === nav.title ? "text-white" : "text-secondary"
-              } hover:text-white text-[18px] font-medium cursor-pointer`}
-              onClick={() => setActive(nav.title)}
+              className={`hover:text-secondary text-[18px] font-medium cursor-pointer text-white`}
+              // onClick={() => setActive(nav.title)}
             >
               <a href={`#${nav.id}`}>{nav.title}</a>
             </li>
           ))}
         </ul>
 
-        <div className="sm:hidden flex flex-1 justify-end items-center">
-          <img
+        <div className="sm:hidden flex justify-center items-end flex-col space-y-2 cursor-pointer" onClick={() => setToggle(!toggle)}>
+          {/* <img
             src={toggle ? close : menu}
             alt="menu"
             className="w-[28px] h-[28px] object-contain"
             onClick={() => setToggle(!toggle)}
+          /> */}
+          <div
+            className={`${animate1} bg-white w-6 h-[2px] transition duration-300 ease-in`}
+          />
+          <div
+            className={`${animate2} bg-white w-6 h-[2px] transition duration-300 ease-in`}
           />
 
           <div
@@ -76,12 +80,10 @@ const Navbar = () => {
               {navLinks.map((nav) => (
                 <li
                   key={nav.id}
-                  className={`font-poppins font-medium cursor-pointer text-[16px] ${
-                    active === nav.title ? "text-white" : "text-secondary"
-                  }`}
+                  className={`font-poppins font-medium cursor-pointer text-[16px] hover:text-secondary`}
                   onClick={() => {
                     setToggle(!toggle);
-                    setActive(nav.title);
+                    // setActive(nav.title);
                   }}
                 >
                   <a href={`#${nav.id}`}>{nav.title}</a>
